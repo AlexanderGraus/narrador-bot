36da2085830dd7124b9996e35831f404665e605b
Alexander Graus <alexander.graus@bigbox.com.ar>
2023-12-22 15:18:30 -0300

    fix: langage and formatting

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import os
 import discord
-import random
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,27 +14,22 @@ async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{client.user} estas conectador al servidor:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
     members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    print(f'miembros:\n - {members}')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
     if message.content == 'quiero escribir!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send("Hola bienvenido al bot de crear historias!")
+        await message.channel.send(
+            "Hola bienvenido al bot para crear historias!"
+        )
+
 client.run(TOKEN)
