232972d168872dc18dd734fdce97a50eaeab2e8e
Alexander Graus <alexander.graus@bigbox.com.ar>
2023-12-25 16:43:57 -0300

    refactor: added utils functions

--- bot.py
+++ bot.py
@@ -13,32 +13,64 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 libros = {}
 
+CARTAS = {
+    "1a": {
+        'titulo': 'Personaje Hace',
+        'descripcion': '`Un personaje cualquiera realiza una acción que modifica la situación o cambia lugar o tiempo`',
+    },
+    "1b": {
+        'titulo': 'Entonces',
+        'descripcion': '`Un personaje cualquiera realiza una accion que continua el sentido en que se desarrolla la historia`',
+    },
+    "1c": {
+        'titulo': 'Pero',
+        'descripcion': '`Un personaje o situacion cualquiera rompe, interrumpe o cambia el sentido en que se desarrolla la historia`',
+    },
+}
 
-def tirar_carta():
-    return random.choice(('1a', '1b', '1c'))
+
+def tirar_carta(id=None):
+    if not id:
+        id = random.choice(('1a', '1b', '1c'))
+    carta = CARTAS.get(id)
+    carta_formatted = f"{id}: **{carta['titulo']}** \n{carta['descripcion']}"
+    return carta_formatted
 
 
+def escribir_cuento(autor_id, cuento):
+    libros.update({autor_id: {'cuento': cuento}})
+
+
+def buscar_cuento(autor_id):
+    return libros.get(autor_id, {}).get('cuento', '')
+
+
+# ----------- Commands -----------------------------
 @bot.command(name='empezar', help='Comienza el proceso de crear una historia!')
 async def crear_historia(ctx):
     await ctx.send(MENSAJE_DE_BIENVENIDA)
-    libros.update({ctx.author.id: {'cuento': ''}})
-    await ctx.send("Personaje hace: un personaje cualquiera realiza una acción que modifica la situación o cambia lugar o tiempo")
+    escribir_cuento(ctx.author.id, '')
+    await ctx.send(tirar_carta('1a'))
 
 
 @bot.command(name='escribir', help='Despues de recibir una carta agregale texto a tu cuento!')
 async def escribir(ctx, *, texto):
-    cuento = libros.get(ctx.author.id, {}).get('cuento', '')
+    cuento = buscar_cuento(ctx.author.id)
     cuento += texto + '. '
-    libros.update({ctx.author.id: {'cuento': cuento}})
-    await ctx.send(cuento+'\n'+tirar_carta())
+    escribir_cuento(ctx.author.id, cuento)
+    await ctx.send('Nueva carta!\n'+tirar_carta())
 
 
 @bot.command(name='fin', help='Cerra tu historia y mira el cuento terminado!')
 async def cerrar_historia(ctx):
-    await ctx.send('Muy bien, excelente historia! Así quedó tu cuento: \n' + libros.get(ctx.author.id).get('cuento'))
-    libros.update({ctx.author.id: {'cuento': ''}})
+    await ctx.send(
+        f"Muy bien, excelente historia! Así quedó tu cuento: \n \
+        `{buscar_cuento(ctx.author.id)}`"
+    )
+    escribir_cuento(ctx.author.id, '')
 
 
+# ------------------ Error Handler -------------------------------
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
