10bc20b6a229a74dd460c2cdb119a0a3a524d495
Alexander Graus <alexander.graus@bigbox.com.ar>
2023-12-31 17:55:00 -0300

    fix: only shuffle ones and when the cards are empty

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 """
-Discord bot that guide you through the process of creating a story. 
+Discord bot that guide you through the process of creating a story.
 Its based on the game designed by Fer Catz (fercatz.talleres@gmail.com)
 """
 
@@ -16,15 +16,11 @@ MENSAJE_DE_BIENVENIDA = """
     ¡Hola! bienvenido, vamos a jugar a escribir un cuento, en menos de 5 minutos...
     Para eso te voy a ir tirando cartas, usalas como disparador y escribí tu texto, así, como surja, no lo pienses mucho.
     Una vez que sientas que la historia ha terminado pone `!fin` y yo te devuelvo lo que escribiste... te vas a sorprender
-    >>> AHHH! Me olvidaba. El creador de este juego se llama **Fer Catz**, da talleres de escritura creativa y te ayuda con tu proceso, estés donde estés.
-    Podés escribirle un mail: [fercatz.talleres@gmail.com](https://mailto:fercatz.talleres@gmail.com) (o buscarlo en facebook e instagram, eso que usan ustedes los jóvenes) Para agradecerle o sugerirle mejoras... Yo le sugerí que venga a Discord, pero no me hizo caso :(
 """
 MENSAJE_PRIMERA_CARTA = """
     Usá el comando `!carta` para pedirme una... carta, obvio. A continuacion indicame de cuál mazo queres que la saque.
-    Si no pones nada te devuelvo una al azar
-    *psss incluso pódes ponerme la carta especifica que queres*
 
-    Tipo de mazos:
+    Tipo de mazos (asi sabes que elegir):
     1) son los recursos basicos de una narracion. podes sacar siempre de acá, no fallan.
     2) son recursos de inmersion y descripcion de la historia
     3) recursos que enriquecen el orden y complejidad del argumento
@@ -32,12 +28,16 @@ MENSAJE_PRIMERA_CARTA = """
 
     **Tip!** para empezar te aconsejo que elijas sacar del mazo (1) que tiene las estructuras básicas.
 """
-
+CREDITO_FER_CATZ = """
+    \n>>> AHHH! Me olvidaba. El creador de este juego se llama **Fer Catz**, da talleres de escritura creativa y te ayuda con tu proceso, estés donde estés.
+    Podés escribirle un mail: [fercatz.talleres@gmail.com](https://mailto:fercatz.talleres@gmail.com) (o buscarlo en facebook e instagram, eso que usan ustedes los jóvenes) Para agradecerle o sugerirle mejoras... Yo le sugerí que venga a Discord, pero no me hizo caso :(
+"""
 load_dotenv()
 # ----------- Variables -----------------------------
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+mazos_mezclados = {}
 libros = {
     'author_id': {
         'contador_turnos': 0,
@@ -60,23 +60,28 @@ def armar_mazos():
 
 def mezclar_mazos():
     mazos = armar_mazos()
-    mazos_mezclados = {}
     for mazo in mazos:
         cartas = mazos[mazo]
         random.shuffle(cartas)
         mazos_mezclados[mazo] = cartas
-    return mazos_mezclados
 
-def tirar_carta(carta_id=None, mazo_id=None):
-    if not carta_id:
-        if mazo_id:
-            mazo = [carta_id for carta_id in CARTAS.keys() if carta_id[0] == mazo_id]
-        else:
-            mazo = list(CARTAS.keys())
-        carta_id = random.choice(mazo) if mazo else None
+def borrar_carta_del_mazo(carta_id):
+    mazo_id = carta_id[0]
+    cartas_del_mazo = mazos_mezclados[mazo_id]
+    cartas_del_mazo.pop(0)
+    if len(cartas_del_mazo) == 0:
+        nuevo_mazo = armar_mazos()[mazo_id]
+        random.shuffle(nuevo_mazo)
+        mazos_mezclados.update({mazo_id: nuevo_mazo})
+
+
+def tirar_carta(mazo_id=None, carta_id=None):
+    if mazo_id:
+        carta_id = mazos_mezclados[mazo_id][0]
     carta = CARTAS.get(carta_id, {})
     if not carta:
-        return "Esa carta no exite, pa, si queres mas cartas... Inventalas"
+        raise Exception("Esa carta no exite, pa, si queres mas cartas... Inventalas")
+    borrar_carta_del_mazo(carta_id)
     return f"{carta_id}: **{carta['titulo']}** \n{carta['descripcion']}"
 
 
@@ -111,26 +116,18 @@ async def crear_historia(ctx):
     await ctx.send(MENSAJE_DE_BIENVENIDA)
     escribir_libro(ctx.author.id)
     await ctx.send('Empecemos con la __primera carta!__')
-    await ctx.send(tirar_carta('1a'))
+    await ctx.send(tirar_carta(carta_id='1a'))
     await ctx.send('Ahora poné `!escribir` y a continuacion tu texto para guardar el primer fragmento :)')
 
 
 @bot.command(name='carta', help='Tirar una carta nueva, indicando de cuál mazo queres sacar. En caso de no elegir ninguno saca una al azar de cualquier mazo')
-async def carta(ctx, id=None):
-    if not id:
-        await ctx.send(tirar_carta())
-        return
-    if len(id) > 1:
-        carta_id = id
-        await ctx.send(tirar_carta(carta_id=carta_id))
-    else:
-        mazo_id = int(id)
-        if mazo_id > 4:
-            raise Exception("Para viejo, yo tengo hasta 4 mazos, si queres mas cartas inventalas")
-        turnos = buscar_libro(ctx.author.id).get('contador_turnos', 0)
-        if mazo_id > turnos:
-            await ctx.send('Ah que picaro que sos eligiendo de otro mazo, muy bien las reglas en la literatura estan PARA ROMPERSE')
-        await ctx.send(tirar_carta(mazo_id=str(mazo_id)))
+async def carta(ctx, mazo_id):
+    if int(mazo_id) > 4:
+        raise Exception('Perdon, tengo hasta 4 mazos, por ahora...')
+    turnos = buscar_libro(ctx.author.id).get('contador_turnos', 0)
+    if int(mazo_id) > turnos:
+        await ctx.send('Ah que picaro que sos eligiendo de otro mazo, muy bien las reglas en la literatura estan PARA ROMPERSE')
+    await ctx.send(tirar_carta(mazo_id))
 
 
 @bot.command(name='escribir', help='Despues de recibir una carta agregale texto a tu cuento!')
@@ -151,6 +148,7 @@ async def cerrar_historia(ctx):
     await ctx.send(
         f"Muy bien, excelente historia! Así quedó tu cuento: \n`{buscar_libro(ctx.author.id).get('cuento', '')}`"
     )
+    await ctx.send(CREDITO_FER_CATZ)
     escribir_libro(ctx.author.id)
 
 
